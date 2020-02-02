a7e5c7c54255df1597e08c6ebe9fa12e67799364
uki.rain <uki.rain@gmail.com>
2020-02-02 12:45:09 +0300

    game over

---
+++ Assets/Scripts/PressAnyKey.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PressAnyKey : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {           
+            Debug.Log("Pressed any key");
+            SceneManager.LoadScene("Menu");
+        }
+    }
+    
+    void OnGUI()
+    {               
+        GUI.Box(new Rect(235, 40, 265, 45), "Your time:" + DataManager.timer.ToString("#.##"));
+    }
+}
